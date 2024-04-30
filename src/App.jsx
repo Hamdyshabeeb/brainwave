aed96b92b05b0c7f41428335709257a58671e606
Hamdy Shabeeb <34157559+Hamdyshabeeb@users.noreply.github.com>
2024-04-30 22:35:24 +0300

    initialize brainwave with react ,reactrouter,tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1> hello worled</h1>
+      <h2>
+        {" "}
+        hello world not <del>worled</del>
+      </h2>
+    </div>
+  );
+}
+
+export default App;
