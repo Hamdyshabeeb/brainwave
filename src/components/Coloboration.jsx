a37d28df06ac5ab2d33d542597161d1a35c0d5d7
Hamdy Shabeeb <34157559+Hamdyshabeeb@users.noreply.github.com>
2024-05-11 00:39:59 +0300

    create collaboration section

---
+++ src/components/Coloboration.jsx
@@ -0,0 +1,17 @@
+import Section from "./Section";
+
+function Coloboration() {
+  return (
+    <Section crosses>
+      <section className="max-w-[90%] mx-auto">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
+        </div>
+      </section>
+    </Section>
+  );
+}
+
+export default Coloboration;
